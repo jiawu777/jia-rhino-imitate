f80fa2b0e4dd17062ec5000c4f4da9fed1a2bc5b
Jia Wu <cwu005104@gmail.com>
2025-01-08 00:14:35 +0800

    feature/addPagePolicy Jia: modify pageHeader position

--- src/components/common/PageHeader/PageHeader.jsx
+++ src/components/common/PageHeader/PageHeader.jsx
@@ -1,16 +1,13 @@
 import './PageHeader.scss';
-import bgHeader from '@images/pageHeader/1.jpg';
+import bgHeader from '@images/pageHeader/1.jpg'; //待解決1：div 的 style 無法使用 backgroud-image
 
 const PageHeader = () => {
   const title = '每一個世界 都值得保護';
 
   return (
     <section className="pageHeader">
-      <div
-        className="pageHeader__wrapper"
-        style={{ background: `url(${bgHeader})` }}
-      >
-        {title}
+      <div className="pageHeader__wrapper">
+        <div className="pageHeader__title">{title}</div>
       </div>
     </section>
   );
