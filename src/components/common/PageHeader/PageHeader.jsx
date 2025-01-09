dabed4ed855fd00fa1fc6ab2890496a05e7fa689
Jia Wu <cwu005104@gmail.com>
2025-01-09 21:12:44 +0800

    feature/addPagePolicy Jia: modify pageHeader backgroung-image & font size RWD

--- src/components/common/PageHeader/PageHeader.jsx
+++ src/components/common/PageHeader/PageHeader.jsx
@@ -1,5 +1,5 @@
 import './PageHeader.scss';
-import bgHeader from '@images/pageHeader/1.jpg'; //待解決1：div 的 style 無法使用 backgroud-image
+import bgHeader from '@images/pageHeader/1.jpg'; //待解決1：div 的 style 無法使用 background-image
 
 const PageHeader = () => {
   const title = '每一個世界 都值得保護';
