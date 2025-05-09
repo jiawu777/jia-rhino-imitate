7b5e0bebb8e905d0dbaf886cecd3b19b516b9d35
Jia Wu <cwu005104@gmail.com>
2025-05-10 00:15:15 +0800

    feature/addPagePolicy Jia: 1. usePageTracker改寫至useEffect，改為使用瀏覽器觸發。

--- src/hooks/usePageVisitTracker.js
+++ src/hooks/usePageVisitTracker.js
@@ -10,50 +10,50 @@ const userInfoSchema = z.array(
 const usePageVisitTracker = (currentPage) => {
   const firstRender = useRef(true);
   const intervalIdTimer = useRef(null);
-
-  //取得userInfo
-  const rawUserInfo = localStorage.getItem('userInfo');
-  let parsedUserInfo = [];
-
-  try {
-    const data = rawUserInfo ? JSON.parse(rawUserInfo) : [];
-    parsedUserInfo = userInfoSchema.parse(data);
-  } catch (error) {
-    console.warn('Invalid userInfo format, resetting to empty array.');
-  }
-
-  //如果有userInfo 解開userInfo的JSON檔
-  const userInfoRef = useRef(parsedUserInfo);
-
-  //更新 localStorage
-  const updateLocalStorageData = () => {
-    const hasData = userInfoRef.current.find((item) => item.id === currentPage);
-
-    //若沒有userInfo，新增userInfo
-    if (!hasData) {
-      userInfoRef.current.push({ id: currentPage, count: 0 });
-    }
-
-    localStorage.setItem('userInfo', JSON.stringify(userInfoRef.current)); //JSON.stringify 把資料JSON化
-    //};
-
-    //確保Interval正確
-    clearInterval(intervalIdTimer.current);
-    intervalIdTimer.current = setInterval(() => {
-      userInfoRef.current = userInfoRef.current.map((item) =>
-        item.id === currentPage ? { ...item, count: item.count + 1 } : item
-      );
-      localStorage.setItem('userInfo', JSON.stringify(userInfoRef.current));
-    }, 5000);
-  };
+  const userInfoRef = useRef([]);
 
   useEffect(() => {
+    //SSR 安全檢查
+    if (typeof window === 'undefined') return;
+    try {
+      //取得userInfo
+      const rawUserInfo = localStorage.getItem('userInfo');
+      const data = rawUserInfo ? JSON.parse(rawUserInfo) : [];
+      userInfoRef.current = userInfoSchema.parse(data);
+    } catch (error) {
+      console.warn('Invalid userInfo format, resetting to empty array.');
+      userInfoRef.current = [];
+    }
+
+    //更新 localStorage
+    const updateLocalStorageData = () => {
+      const hasData = userInfoRef.current.find((item) => item.id === currentPage);
+
+      //若沒有userInfo，新增userInfo
+      if (!hasData) {
+        userInfoRef.current.push({ id: currentPage, count: 0 });
+      }
+
+      localStorage.setItem('userInfo', JSON.stringify(userInfoRef.current)); //JSON.stringify 把資料JSON化
+      //};
+
+      //確保Interval正確
+      clearInterval(intervalIdTimer.current);
+      intervalIdTimer.current = setInterval(() => {
+        userInfoRef.current = userInfoRef.current.map((item) =>
+          item.id === currentPage ? { ...item, count: item.count + 1 } : item
+        );
+        localStorage.setItem('userInfo', JSON.stringify(userInfoRef.current));
+      }, 5000);
+    };
     //如果非第一次執行就return不運作
     if (firstRender.current) {
       firstRender.current = false;
       return;
     }
+
     updateLocalStorageData();
+
     return () => clearInterval(intervalIdTimer.current);
   }, [currentPage]);
 };
